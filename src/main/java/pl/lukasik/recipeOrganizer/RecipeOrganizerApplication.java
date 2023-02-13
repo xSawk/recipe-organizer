90e760764832c943b995ea7bdac1701ea806cfcb
xSawk <marcinlukasik1997@gmail.com>
2023-02-13 20:02:59 +0100

    Initial commit

---
+++ src/main/java/pl/lukasik/recipeOrganizer/RecipeOrganizerApplication.java
@@ -0,0 +1,13 @@
+package pl.lukasik.recipeOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lukasik/recipeOrganizer/RecipeOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lukasik.recipeOrganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
